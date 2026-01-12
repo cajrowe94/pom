31da61d0165654e6a0f068af90a6cefbed7dd70a
Caleb Rowe <37968598+cajrowe94@users.noreply.github.com>
2026-01-12 17:52:00 -0500

    exports, anim setup in godot

---
+++ godot/pom/player/player.gd
@@ -0,0 +1,43 @@
+extends CharacterBody2D
+
+@export var player_speed = 300
+@export var gravity = 30
+@export var jump_strength = 700
+
+@onready var _animated_sprite = $AnimatedSprite2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func _physics_process(delta: float) -> void:
+	if !is_on_floor():
+		velocity.y += gravity
+		_animated_sprite.play("jump")
+		print('falling')
+	elif velocity.x == 0:
+		_animated_sprite.play("idle")
+		print('idling')
+	else:
+		#_animated_sprite.play("begin-run")
+		_animated_sprite.play("run")
+		print('running')
+		
+	if Input.is_action_just_pressed("player_jump") && is_on_floor():
+		velocity.y = -jump_strength
+		_animated_sprite.play("jump")
+	
+	var horizontal_direction = Input.get_axis("player_move_left", "player_move_right")
+	
+	velocity.x = player_speed * horizontal_direction
+	
+	if horizontal_direction < 0:
+		_animated_sprite.flip_h = true;
+	elif horizontal_direction > 0:
+		_animated_sprite.flip_h = false;
+	
+	move_and_slide()
