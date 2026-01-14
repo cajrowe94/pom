0b8cdaf042f25a323ec70a40100489654901548f
Caleb Rowe <37968598+cajrowe94@users.noreply.github.com>
2026-01-14 14:44:46 -0500

    test level, some jump logic

--- godot/pom/player/player.gd
+++ godot/pom/player/player.gd
@@ -3,8 +3,13 @@ extends CharacterBody2D
 @export var player_speed = 300
 @export var gravity = 30
 @export var jump_strength = 700
+@export var max_jump = 1.0
 
 @onready var _animated_sprite = $AnimatedSprite2D
+
+var jump_key_duration = 0.0
+var ended_jump = true
+var released_jump_key = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,16 +21,36 @@ func _process(delta: float) -> void:
 	
 func _physics_process(delta: float) -> void:
 	if !is_on_floor():
-		velocity.y += gravity
+		# jump key duration determines height of jump
+		if Input.is_action_pressed("player_jump") && jump_key_duration < max_jump:
+			# jump higher
+			velocity.y -= jump_key_duration
+			jump_key_duration += 0.1
+		elif Input.is_action_pressed('player_jump') && released_jump_key:
+			velocity.y += 0
+		else:
+			# end jump, fall to floor at full speed
+			#ended_jump = true
+			#jump_key_duration = 0.0
+			velocity.y += gravity
 		_animated_sprite.play("jump")
 	elif velocity.x == 0:
+		#released_jump_key = true
 		_animated_sprite.play("idle")
 	else:
+		#released_jump_key = true
 		_animated_sprite.play("run")
 		
 	if Input.is_action_just_pressed("player_jump") && is_on_floor():
+		#ended_jump = false
+		released_jump_key = false
+		jump_key_duration = 0.0
 		velocity.y = -jump_strength
 		_animated_sprite.play("jump")
+	
+	if Input.is_action_just_released('player_jump'):
+		released_jump_key = true
+		
 	
 	var horizontal_direction = Input.get_axis("player_move_left", "player_move_right")
 	
