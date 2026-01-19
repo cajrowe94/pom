9b7e04fe2042dc100ccfed3cdd8158e42041adea
Caleb Rowe <37968598+cajrowe94@users.noreply.github.com>
2026-01-19 03:01:58 -0500

    new anims, player control logic -- jump, dbl jump

--- godot/pom/player/player.gd
+++ godot/pom/player/player.gd
@@ -3,55 +3,118 @@ extends CharacterBody2D
 @export var player_speed = 300
 @export var gravity = 30
 @export var jump_strength = 700
-@export var max_jump = 1.0
+@export var max_jump_boost = 1.0
+@export var max_jumps = 2
 
 @onready var _animated_sprite = $AnimatedSprite2D
 
 var jump_key_duration = 0.0
-var ended_jump = true
-var released_jump_key = true
+var jump_count = 0
+var has_initiated_double_jump = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	_animated_sprite.connect("animation_finished", handle_animation_finished)
+	
+	if is_on_floor():
+		_animated_sprite.play("idle")
+	else:
+		_animated_sprite.play("fall")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 	
 func _physics_process(delta: float) -> void:
+	if is_on_floor():
+		reset_jump()
+
+	if Input.is_action_just_pressed("player_jump"):
+		handle_jump_key_press()
+	
 	if !is_on_floor():
+		if _animated_sprite.animation == "run":
+			_animated_sprite.play("jump")
+
+		var max_jump_boost_reached = jump_key_duration >= max_jump_boost
+		var max_jumps_reached = jump_count > max_jumps
 		# jump key duration determines height of jump
-		if Input.is_action_pressed("player_jump") && jump_key_duration < max_jump:
-			# jump higher
-			velocity.y -= jump_key_duration
-			jump_key_duration += 0.1
-		elif Input.is_action_pressed('player_jump') && released_jump_key:
-			velocity.y += 0
+		
+		if Input.is_action_pressed("player_jump") && !max_jump_boost_reached && !max_jumps_reached:
+			if jump_count == 1:
+				# first jump
+				player_jump(jump_key_duration)
+				jump_key_duration += 0.1
+			else:
+				# double jump
+				if !has_initiated_double_jump:
+					velocity.y = -jump_strength
+					has_initiated_double_jump = true
+				else:
+					player_double_jump(jump_key_duration)
+					jump_key_duration += 0.04
 		else:
-			# end jump, fall to floor at full speed
-			#ended_jump = true
-			#jump_key_duration = 0.0
-			velocity.y += gravity
-		_animated_sprite.play("jump")
-	elif velocity.x == 0:
-		#released_jump_key = true
-		_animated_sprite.play("idle")
-	else:
-		#released_jump_key = true
+			player_fall()
+	elif velocity.x != 0:
 		_animated_sprite.play("run")
-		
-	if Input.is_action_just_pressed("player_jump") && is_on_floor():
-		#ended_jump = false
-		released_jump_key = false
-		jump_key_duration = 0.0
-		velocity.y = -jump_strength
-		_animated_sprite.play("jump")
+	else:
+		_animated_sprite.play("idle")
 	
-	if Input.is_action_just_released('player_jump'):
-		released_jump_key = true
-		
-	
+	player_run()
+	move_and_slide()
+
+# animation_finished callback
+func handle_animation_finished() -> void:
+	print(_animated_sprite.animation)
+	match _animated_sprite.animation:
+		"jump", "fall":
+			if !is_on_floor():
+				_animated_sprite.play("fall")
+		"double_jump":
+			if !is_on_floor():
+				_animated_sprite.play("jump")
+				
+			
+
+# user presses jump key
+func handle_jump_key_press() -> void:
+	jump_key_duration = 0.0
+	jump_count += 1
+	# perform initial jump
+	if is_on_floor():
+		player_jump(jump_strength)
+	elif !is_on_floor() && jump_count == 1: # we jumped after walking off an edge
+		jump_count += 1
+
+
+# reset vars for jump logic
+func reset_jump() -> void:
+	jump_count = 0
+	has_initiated_double_jump = false
+	jump_key_duration = 0.0
+
+# add gravity value to player y velocity
+func player_fall() -> void:
+	#_animated_sprite.play('fall')
+	velocity.y += gravity
+	#if _animated_sprite.animation != "double_jump":
+
+# subtract a value (float) from player's y velocity
+func player_jump(jump_val: float) -> void:
+	_animated_sprite.play("jump")
+	velocity.y -= jump_val
+
+# when player uses double jump ability
+func player_double_jump(jump_val: float) -> void:
+	_animated_sprite.play("double_jump")
+	velocity.y -= jump_val
+
+# when player is not moving on the x axis
+#func player_idle() -> void:
+	#_animated_sprite.play("idle")
+
+# when player is moving on the x axis
+func player_run() -> void:
 	var horizontal_direction = Input.get_axis("player_move_left", "player_move_right")
 	
 	velocity.x = player_speed * horizontal_direction
@@ -60,5 +123,3 @@ func _physics_process(delta: float) -> void:
 		_animated_sprite.flip_h = true;
 	elif horizontal_direction > 0:
 		_animated_sprite.flip_h = false;
-	
-	move_and_slide()
